2bd516718eb9cc6ede9869e367ccb5d80a1babb5
LeonidasMbm <leonidas.mbuembue@chift.eu>
2023-03-30 21:57:38 +0200

    deploy for linux/macOs

--- install.bat
+++ install.bat
@@ -1,13 +1,13 @@
 @echo off
 
 REM Vérifie si Python 3.10 et pip sont installés, sinon les installe automatiquement
-python --version 2>nul
+python --version > nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python 3.10 n'est pas installé. Installation en cours...
-    curl -s -o python_installer.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
-    start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
-    del python_installer.exe
-    echo Python 3.10 installé avec succès.
+  echo Python 3.10 n'est pas installé. Téléchargement...
+  powershell -Command "Invoke-WebRequest -OutFile python310.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe"
+  echo Installation de Python 3.10...
+  start /wait python310.exe /quiet InstallAllUsers=1 PrependPath=1
+  SETX PATH "%PATH%;%USERPROFILE%\AppData\Local\Programs\Python310\python.exe" /M
 ) else (
     echo Python 3.10 est déjà installé.
 )
@@ -23,11 +23,14 @@ if %errorlevel% neq 0 (
     echo pip est déjà installé.
 )
 
+cd /d %~dp0
 REM Installe le fichier .tar.gz depuis le dossier dist
 echo Installation de l'application en cours...
 cd dist
-python3 -m pip install beatstemp-0.1.tar.gz
+pip install beatstemp-0.1.tar.gz
 cd ..
 echo Installation terminée.
 
 pause
+
+
